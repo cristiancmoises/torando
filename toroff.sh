3779327cd7fe88d3d8e655bccba5fb743780a273
Cristian Cezar Moisés <86272521+cristiancmoises@users.noreply.github.com>
2023-12-04 21:08:59 -0300

    Update toroff.sh

--- toroff.sh
+++ toroff.sh
@@ -1,5 +1,5 @@
-sudo iptables -t nat -A OUTPUT -p tcp -m owner --uid-owner USERAQUI -m tcp -j RED>
-sudo iptables -t nat -A OUTPUT -p udp -m owner --uid-owner USERAQUI -m udp --dpor>
-sudo iptables -t filter -A OUTPUT -p tcp -m owner --uid-owner USERAQUI -m tcp --d>
-sudo iptables -t filter -A OUTPUT -p udp -m owner --uid-owner USERAQUI -m udp --d>
-sudo iptables -t filter -A OUTPUT -m owner --uid-owner USERAQUI -j DROP
+    sudo iptables -t nat -D OUTPUT -p tcp -m owner --uid-owner USERAQUI -m tcp -j REDIRECT --to-ports 9040
+    sudo iptables -t nat -D OUTPUT -p udp -m owner --uid-owner USERAQUI -m udp --dport 53 -j REDIRECT --to-ports 53
+    sudo iptables -t filter -D OUTPUT -p tcp -m owner --uid-owner USERAQUI -m tcp --dport 9040 -j ACCEPT
+    sudo iptables -t filter -D OUTPUT -p udp -m owner --uid-owner USERAQUI -m udp --dport 53 -j ACCEPT
+    sudo iptables -t filter -D OUTPUT -m owner --uid-owner USERAQUI -j DROP
